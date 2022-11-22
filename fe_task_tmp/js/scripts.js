f9f88d2764edae080f96069ba9406ec5aa0a8a93
Eduard Dietzel <banderol.ed@gmail.com>
2022-11-22 10:26:59 +0100

    erneuerung alle files

---
+++ fe_task_tmp/js/scripts.js
@@ -0,0 +1,13 @@
+console.log('test');
+
+var value = 'value';
+
+console.log(value);
+
+var button = document.getElementById('button-id');
+
+var popup = document.getElementById('popup-id');
+
+console.log(button, popup);
+
+console.log(button.textContent);
